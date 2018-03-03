3ce8c7d578ac35093a4c82733797747c687f5dc8
Addison Schuhardt <aschuhardt@users.noreply.github.com>
2018-03-03 13:21:46 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ailurus
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameState())
+                game.Run();
+        }
+    }
+}
